98bb83a48828e07ed3e6187d4e2c6506ef48d17f
Abdullaah <abdullahalnirob12@gmail.com>
2025-10-20 10:08:11 +0600

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import About from '@/components/About';
 import Benefits from '@/components/Benefits';
+import DashboardCard from '@/components/DashboardCard';
 import HomeNavbar from '@/components/HomeNavbar';
+import Team from '@/components/Team';
 import Home from '@/pages/home';
 import React from 'react';
 
@@ -11,6 +13,8 @@ const Page = () => {
       <Home />
       <About />
       <Benefits />
+      <DashboardCard />
+      <Team />
     </div>
   );
 };
